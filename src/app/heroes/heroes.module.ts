f8d2ef26c7a4187193bd9a84d2370fa63decae46
GCastro68 <gcastro2805@gmail.com>
2021-09-14 19:57:29 -0600

    Fin App Heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -9,6 +9,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HeroeComponent,
     HomeComponent,
     ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe,
   ],
   imports: [
     CommonModule,
