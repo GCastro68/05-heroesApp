0d9c2a02c2270074e0ea99eb15ad819d5f71fe93
GCastro68 <gcastro2805@gmail.com>
2021-09-20 20:41:57 -0600

    Excepciones Imane pipe

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -6,6 +6,15 @@ import { Heroe } from '../interfaces/heroes.interface';
 })
 export class ImagenPipe implements PipeTransform {
   transform(heroe: Heroe): string {
+    if (!heroe.id && !heroe.alt_img) {
+      return 'assets/no-image.png';
+    } else if (heroe.alt_img) {
+      return heroe.alt_img;
+    }
+
+    if (heroe.id && heroe.alt_img !== undefined) {
+      return 'assets/no-image.png';
+    }
     return `assets/heroes/${heroe.id}.jpg`;
   }
 }
